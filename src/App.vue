9ccb22f201ea6b4c2e5289687f84fa8f181ae3e8
InsertIntoHTL <5125@htl.rennweg.at>
2021-04-18 19:08:24 +0200

    streamhase

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Over class="overlay"></Over>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Over from './components/Over.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Over
   }
 }
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Oswald', sans-serif;
+  }
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
+
+
+  .overlay {
+    z-index: 5;
+  }
 </style>
