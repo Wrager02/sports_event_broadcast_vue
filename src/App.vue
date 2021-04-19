3fe6a8c04412d35f1688b079d011a3978875a817
Wrager02 <alre0209@gmail.com>
2021-04-19 20:02:44 +0200

    Useless tests

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from "@/components/Test";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test,
   }
 }
 </script>
@@ -24,5 +23,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: black;
 }
 </style>
